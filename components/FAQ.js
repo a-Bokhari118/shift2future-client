473230216a3917b248d8b8849f0109a57a9ad9b4
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-12-10 20:05:09 +0300

    :hammer: fix contactus

--- components/FAQ.js
+++ components/FAQ.js
@@ -1,80 +1,82 @@
 const FAQ = () => {
   return (
     <section className="py-16 bg-[#F4F4F4] relative">
-      <h1 className="text-3xl md:text-5xl lg:text-6xl text-center font-extrabold mb-5 text-gray-500 ">
-        FAQ
-      </h1>
+      <div className="container mx-auto">
+        <h1 className="text-3xl md:text-5xl lg:text-6xl text-center font-extrabold mb-5 text-gray-500 ">
+          FAQ
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:mx-40 mx-4 mt-20 mb-40">
+          <div className="text-gray-800  bg-gray-100  rounded-md p-4">
+            <h2 className="text-md font-extrabold">
+              What does Lifetime Access exactly mean?
+            </h2>
+            <p className="text-sm mt-4 font-light leading-6">
+              Tailwind Master Kits' lifetime access gives you access to the
+              components and the code with no additional recurring charges. Pay
+              once, use the components forever. Also, if in the future more
+              components are added, you get access to them if you're subscribed
+              for the lifetime access membership.
+            </p>
+          </div>
+          <div className="text-gray-800  bg-gray-100  rounded-md p-4">
+            <h2 className="text-md font-extrabold">
+              How is One-Year access pass different from Lifetime Access?
+            </h2>
+            <p className="text-sm mt-4 font-light leading-6">
+              With One-Year pass, you get access to all the premium components
+              for One Year and all the updates are for One Year only. With
+              Lifetime Access, you get access to the entire set for features for
+              Lifetime - Which means you can get future updates for the
+              components as long as the website is live.
+            </p>
+          </div>
+          <div className="text-gray-800  bg-gray-100  rounded-md p-4">
+            <h2 className="text-md font-extrabold">
+              Can I resell the components on my own website?
+            </h2>
+            <p className="text-sm mt-4 font-light leading-6">
+              No. You cannot resell the components on your own website claiming
+              that you built them. You cannot create templates out of Tailwind
+              Master Kit's components and sell them. You can use the components
+              for client projects and personal use.
+            </p>
+          </div>
+          <div className="text-gray-800  bg-gray-100  rounded-md p-4">
+            <h2 className="text-md font-extrabold">What are Free Updates?</h2>
+            <p className="text-sm mt-4 font-light leading-6">
+              We are tirelessly working on adding more quality components on the
+              platform. Most of our components are premium but we do release
+              free components and keep on updating them regularly. Lookout for
+              our newsletter, we release new components every month.
+            </p>
+          </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:mx-40 mx-4 mt-20 mb-40">
-        <div className="text-gray-800  bg-gray-100  rounded-md p-4">
-          <h2 className="text-md font-extrabold">
-            What does Lifetime Access exactly mean?
-          </h2>
-          <p className="text-sm mt-4 font-light leading-6">
-            Tailwind Master Kits' lifetime access gives you access to the
-            components and the code with no additional recurring charges. Pay
-            once, use the components forever. Also, if in the future more
-            components are added, you get access to them if you're subscribed
-            for the lifetime access membership.
-          </p>
-        </div>
-        <div className="text-gray-800  bg-gray-100  rounded-md p-4">
-          <h2 className="text-md font-extrabold">
-            How is One-Year access pass different from Lifetime Access?
-          </h2>
-          <p className="text-sm mt-4 font-light leading-6">
-            With One-Year pass, you get access to all the premium components for
-            One Year and all the updates are for One Year only. With Lifetime
-            Access, you get access to the entire set for features for Lifetime -
-            Which means you can get future updates for the components as long as
-            the website is live.
-          </p>
-        </div>
-        <div className="text-gray-800  bg-gray-100  rounded-md p-4">
-          <h2 className="text-md font-extrabold">
-            Can I resell the components on my own website?
-          </h2>
-          <p className="text-sm mt-4 font-light leading-6">
-            No. You cannot resell the components on your own website claiming
-            that you built them. You cannot create templates out of Tailwind
-            Master Kit's components and sell them. You can use the components
-            for client projects and personal use.
-          </p>
-        </div>
-        <div className="text-gray-800  bg-gray-100  rounded-md p-4">
-          <h2 className="text-md font-extrabold">What are Free Updates?</h2>
-          <p className="text-sm mt-4 font-light leading-6">
-            We are tirelessly working on adding more quality components on the
-            platform. Most of our components are premium but we do release free
-            components and keep on updating them regularly. Lookout for our
-            newsletter, we release new components every month.
-          </p>
-        </div>
+          <div className="text-gray-800  bg-gray-100  rounded-md p-4">
+            <h2 className="text-md font-extrabold">
+              On how many projects can I use Tailwind Master Kit's components?
+            </h2>
+            <p className="text-sm mt-4 font-light leading-6">
+              With Tailwind Master Kit's Lifetime Access Pass, you can use the
+              components on as many websites as you want - which is unlimited.
+              As long as its just you who is using the licence, use it on
+              unlimited projects.
+            </p>
+          </div>
 
-        <div className="text-gray-800  bg-gray-100  rounded-md p-4">
-          <h2 className="text-md font-extrabold">
-            On how many projects can I use Tailwind Master Kit's components?
-          </h2>
-          <p className="text-sm mt-4 font-light leading-6">
-            With Tailwind Master Kit's Lifetime Access Pass, you can use the
-            components on as many websites as you want - which is unlimited. As
-            long as its just you who is using the licence, use it on unlimited
-            projects.
-          </p>
-        </div>
-
-        <div className="text-gray-800  bg-gray-100  rounded-md p-4">
-          <h2 className="text-md font-extrabold">
-            What is community access and how is it beneficial for me?
-          </h2>
-          <p className="text-sm mt-4 font-light leading-6">
-            You get access to our private Discord Server where you can request
-            for custom components, seek help from the makers of Tailwind Master
-            Kit and other connect and network with other like minded people who
-            are already using the product.
-          </p>
+          <div className="text-gray-800  bg-gray-100  rounded-md p-4">
+            <h2 className="text-md font-extrabold">
+              What is community access and how is it beneficial for me?
+            </h2>
+            <p className="text-sm mt-4 font-light leading-6">
+              You get access to our private Discord Server where you can request
+              for custom components, seek help from the makers of Tailwind
+              Master Kit and other connect and network with other like minded
+              people who are already using the product.
+            </p>
+          </div>
         </div>
       </div>
+
       <div className="wave2 ">
         <svg
           data-name="Layer 1"
