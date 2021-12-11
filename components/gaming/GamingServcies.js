7e4d8bf1f00ce0047a5786ab054f214266a67707
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-12-11 12:48:29 +0300

    :hammer: gaming section p-1

---
+++ components/gaming/GamingServcies.js
@@ -0,0 +1,13 @@
+const GamingServcies = () => {
+  return (
+    <section className="  p-5 bg-[#F4F4F4] z-20 relative">
+      <div className="container mx-auto flex flex-col items-center justify-center mb-20">
+        <h1 className="text-3xl md:text-5xl lg:text-6xl text-center font-extrabold mb-5 text-gray-500 ">
+          What We Offer
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default GamingServcies;
