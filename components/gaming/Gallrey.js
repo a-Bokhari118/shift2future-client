559b136b73c127d4aa611408f890e3fa92cba38d
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-12-15 23:06:08 +0300

    :hammer: fixing footer

--- components/gaming/Gallrey.js
+++ components/gaming/Gallrey.js
@@ -5,7 +5,7 @@ const Gallrey = () => {
         Gaming Setups We Made
       </h1>
       <div class="container mx-auto">
-        <div class="grid-cols-3 p-20 space-y-2 bg-[#F4F4F4] lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+        <div class="grid-cols-3 p-0 lg:p-20 space-y-2 bg-[#F4F4F4] lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
           <div class="w-full rounded">
             <img
               src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
--- components/gaming/ServiceDesc.js
+++ components/gaming/ServiceDesc.js
@@ -5,7 +5,7 @@ const ServiceDesc = () => {
   return (
     <header className="px-4 relative z-20 py-16 w-full bg-[#F4F4F4]">
       <div className="flex flex-col lg:flex-row items-center container mx-auto gap-x-10 ">
-        <div className="order-2 text-gray-900 lg:pt-0 w-full  ">
+        <div className="order-2 text-gray-900 lg:pt-0 w-full pt-10 ">
           <h2 className="text-3xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-3xl font-extrabold uppercase transform ">
             Creating or upgrading your gaming/office setups
           </h2>
