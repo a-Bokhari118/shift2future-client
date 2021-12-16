aa5799b8dec2ce158a9363746ad7550b059b9aad
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-12-16 11:51:30 +0300

    :hammer: gallery responsive

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const Gallery = () => {
   return (
     <section className="text-gray-600 body-font relative">
@@ -7,50 +9,68 @@ const Gallery = () => {
             Our Work
           </h1>
         </div>
-        <div className="flex flex-wrap md:-m-2 -m-1">
+        <div className="flex flex-wrap md:-m-2 -m-1 w-full">
           <div className="flex flex-wrap w-1/2">
-            <div className="md:p-2 p-1 w-1/2">
-              <img
+            <div className="relative md:p-2 p-1 w-1/2">
+              <Image
                 alt="gallery"
                 className="w-full object-cover h-full object-center block"
                 src="https://www.gamingdebugged.com/wp-content/uploads/2021/09/purple-gaming-set-up-1.jpg"
+                layout="responsive"
+                height={1080}
+                width={1920}
               />
             </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <img
+            <div className="relative md:p-2 p-1 w-1/2">
+              <Image
                 alt="gallery"
                 className="w-full object-cover h-full object-center block"
                 src="http://afrienergyonline.com/wp-content/uploads/2019/05/89c665bf9f3353b311e8960f418e3210.jpg"
+                layout="responsive"
+                height={1080}
+                width={1920}
               />
             </div>
-            <div className="md:p-2 p-1 w-full">
-              <img
+            <div className="relative md:p-2 p-1 w-full">
+              <Image
                 alt="gallery"
                 className="w-full h-full object-cover object-center block"
                 src="https://1.bp.blogspot.com/-5uxdFJeQ69I/YIhHwQuPV6I/AAAAAAAAD3k/RUKfvB9kp0wMO_1n0n-oeDVQpYLF97EGQCLcBGAsYHQ/s1280/1.jpg"
+                layout="responsive"
+                height={1080}
+                width={1920}
               />
             </div>
           </div>
           <div className="flex flex-wrap w-1/2">
-            <div className="md:p-2 p-1 w-full">
-              <img
+            <div className="relative md:p-2 p-1 w-full">
+              <Image
                 alt="gallery"
                 className="w-full h-full object-cover object-center block"
                 src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/165146185/original/2cf72b5e8d9a5ae489cbed6463cd9c0dcbc7c5d1/design-you-a-pc-setup.jpg"
+                layout="responsive"
+                height={1080}
+                width={1920}
               />
             </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <img
+            <div className="relative md:p-2 p-1 w-1/2">
+              <Image
                 alt="gallery"
                 className="w-full object-cover h-full object-center block"
-                src="https://miro.medium.com/max/1400/1*W6wAfqHHjMr3jbaZ99DKtQ.png"
+                src="http://afrienergyonline.com/wp-content/uploads/2019/05/89c665bf9f3353b311e8960f418e3210.jpg"
+                layout="responsive"
+                height={1080}
+                width={1920}
               />
             </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <img
+            <div className="relative md:p-2 p-1 w-1/2">
+              <Image
                 alt="gallery"
                 className="w-full object-cover h-full object-center block"
                 src="https://www.deephouseamsterdam.com/wp-content/uploads/2015/08/Kinetic_Light_Featured.jpg"
+                layout="responsive"
+                height={1080}
+                width={1920}
               />
             </div>
           </div>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['www.gamingdebugged.com'],
+    domains: [
+      'www.gamingdebugged.com',
+      'afrienergyonline.com',
+      '1.bp.blogspot.com',
+      'fiverr-res.cloudinary.com',
+      'miro.medium.com',
+      'www.deephouseamsterdam.com',
+    ],
   },
 };
