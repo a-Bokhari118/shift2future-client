d1108ea2b2847e3acb914b5d699266461efa2690
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-12-18 18:12:47 +0300

    :hammer: setup server for dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import Layout from '@components/Layout';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  if (router.pathname.startsWith('/admin')) {
+    return <Component {...pageProps} />;
+  }
   return (
     <Layout>
       <Component {...pageProps} />
